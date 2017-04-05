a8a5a69e8172936894024a2f2a89fbd8c972eab8
max <hamburger@126.com>
2017-04-05 22:30:27 +0800

    增加Android原生录制方法

--- app/src/main/java/learn/bobo/com/video/MainActivity.java
+++ app/src/main/java/learn/bobo/com/video/MainActivity.java
@@ -1,22 +1,30 @@
 package learn.bobo.com.video;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.sf_video)
+    SurfaceView mSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -54,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_start)
     public void startRecord(){
-
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
     }
 }
